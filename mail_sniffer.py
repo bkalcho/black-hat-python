e1a4ac189dae68fbb9d71eaf91aa7da665153852
Bojan Kalicanin <kalcho@zoho.com>
2017-04-14 00:09:26 +0200

    added mail sniffer

---
+++ mail_sniffer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+__author__ = 'kalcho'
+
+from scapy.all import *
+
+# our packet callback
+def packet_callback(packet):
+    print packet.show()
+
+
+# fire up our sniffer
+sniff(prn=packet_callback, count=1)
