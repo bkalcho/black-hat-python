9b94e8f5173d270383f6e46e7c00c11b7b23fbfa
Bojan Kalicanin <kalcho@zoho.com>
2017-04-15 14:19:07 +0200

    added face detection

--- pic_carver.py
+++ pic_carver.py
@@ -59,6 +59,27 @@ def extract_image(headers, http_payload):
     return image, image_type
 
 
+def face_detect(path, file_name):
+
+    img = cv2.imread(path)
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+    rects = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE,
+        (20, 20))
+    
+    if len(rects) == 0:
+        return False
+    
+    rects[:, 2:] += rects[:, :2]
+
+    # highlighting the faces in the image
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+    
+    cv2.imwrite("%s/%s-%s" % (faces_directory, pcap_file, file_name), img)
+
+    return True
+
+
 def http_assembler(pcap_file):
 
     carved_images = 0
