f7968db5583d1b6dad1d5dd6ebcd240428ccb7a5
Bojan Kalicanin <kalcho@zoho.com>
2017-05-31 16:18:26 +0200

    housekeeping

---
+++ decryptor.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+__author__ = 'kalcho'
+
+
+import zlib
+import base64
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+
+private_key = "###PASTE PRIVATE KEY HERE###"
+
+rsakey = RSA.importKey(private_key)
+rsakey = PKCS1_OAEP.new(rsakey)
+
+chunk_size = 256
+offset = 0
+decrypted = ""
+encrypted = base64.b64decode(encrypted)
+
+while offset < len(encrypted):
+    decrypted += rsakey.decrypt(encrypted[offset:offset+chunk_size])
+    offset += chunk_size
+
+# now we decompress to original
+plaintext = zlib.decompress(decrypted)
+
+print plaintext
